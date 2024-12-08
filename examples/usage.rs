b6ac52e1875c2dbc65866ccb9745523e4e8c37df
uncenter <47499684+uncenter@users.noreply.github.com>
2024-12-08 18:06:17 -0500

    chore: update example

--- examples/usage.rs
+++ examples/usage.rs
@@ -1,12 +1,11 @@
 fn main() {
-    println!("{:?}", user_dirs::config_dir());
+    std::env::set_var("XDG_CONFIG_HOME", "foo");
+    println!("With XDG_CONFIG_HOME set: {:?}", user_dirs::config_dir());
 
     std::env::remove_var("XDG_CONFIG_HOME");
-
+    println!("With XDG_CONFIG_HOME unset: {:?}", user_dirs::config_dir());
     assert_eq!(
         user_dirs::config_dir().unwrap(),
         user_dirs::os::config_dir().unwrap()
     );
-
-    println!("{:?}", user_dirs::config_dir());
 }
